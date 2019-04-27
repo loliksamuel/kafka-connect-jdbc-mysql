979e9666a4d92ef41ec56ac233fa14280729cfe1
Samuel Cardonis <o528060707@gmail.com>
2019-04-27 20:39:17 +0300

    add sink+scripts

--- scripts/connectors/delete.sh
+++ scripts/connectors/delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec connect curl -X DELETE https://connect:8083/connectors/sink_postgres
-docker exec connect curl -X DELETE https://connect:8083/connectors/src_mysql_ts
-docker exec connect curl -X DELETE https://connect:8083/connectors/src_mysql_bulk
-docker exec connect curl -X DELETE https://connect:8083/connectors/src_mysql_txn
+docker exec connect curl -X DELETE localhost:8083/connectors/sink_postgres|jq
+docker exec connect curl -X DELETE localhost:8083/connectors/src_mysql_ts|jq
+docker exec connect curl -X DELETE localhost:8083/connectors/src_mysql_bulk|jq
+docker exec connect curl -X DELETE localhost:8083/connectors/src_mysql_txn|jq
